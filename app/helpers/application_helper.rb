df8a6fdcbd37ffda32c7b788ab75fdf779afb89b
Andy Stechishin <andy.stechishin@gmail.com>
2016-10-24 09:23:11 -0600

    Fixes #5, #9, #8, #4
    use markdown formatting for example descriptions in list views, add method to limit example descriptions to 200 (ish) characters, removed the italics from the supporting text for all list views, swapped the positioning of the detail and download link on the example search and section show screens, add comment display to example show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,43 @@ module ApplicationHelper
     @lexer ||= Rouge::Lexers::XML.new
     @formatter.format(@lexer.lex(content))
   end
+
+  def truncate_words(text, length=50, trailing=nil)
+    if text.length < length
+      text
+    else
+      full_words = ''
+      text.split(/\s+/).each do |word|
+        if (full_words.length + word.length/2) < length
+          full_words << ' ' << word
+        end
+      end
+      if trailing
+        full_words << trailing
+      end
+      full_words
+    end
+  end
+
+  def truncate_md(content, length=190, trailing='...')
+    if content.length < length
+      content
+    else
+      offset = 0
+      truncated_content = nil
+      while truncated_content.nil?
+        if content[length - offset] =~ /\s/
+          truncated_content =
+              content.slice(0, length - offset) + (trailing.nil? ? '' : trailing)
+        elsif length + offset >= content.length
+          truncated_content = content
+        elsif content[length + offset] =~ /\s/
+          truncated_content =
+              content.slice(0, length + offset) + (trailing.nil? ? '' : trailing)
+        end
+        offset += 1
+      end
+      truncated_content
+    end
+  end
 end
