3e65f74b21a60b4ee70980a42628f2f2f7fcbad5
Andy Stechishin <andy.stechishin@gmail.com>
2016-11-21 20:42:24 -0700

    Add GitHub event to repository save

--- app/controllers/api/repository_controller.rb
+++ app/controllers/api/repository_controller.rb
@@ -2,7 +2,7 @@ class Api::RepositoryController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
-    model = RepoMessage.new(action: 'Test', body: params.to_s)
+    model = RepoMessage.new(action: request.headers['HTTP_X_GitHub_Event'], body: params.to_s)
     model.save!
     render text: 'OK'
   end
