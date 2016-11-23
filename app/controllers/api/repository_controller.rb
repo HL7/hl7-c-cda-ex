866e479b1061c0f284aad93dffc2e640ea0f336e
Andy Stechishin <andy.stechishin@gmail.com>
2016-11-23 13:11:39 -0700

    Extend repo_message to possibly serve as job control
    Refine repo_messages to include adds, add_status, changed, and changed_status; rework repository_controller to log only pushes to master and require the presence of the head_commit

--- app/controllers/api/repository_controller.rb
+++ app/controllers/api/repository_controller.rb
@@ -2,8 +2,18 @@ class Api::RepositoryController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
-    model = RepoMessage.new(action: request.headers['HTTP_X_GitHub_Event'], body: params.to_s)
-    model.save!
+    if params.include?('ref') && params['ref'] == 'refs/heads/master'
+      if params.include?('head_commit')
+        message = params['head_commit'].include?('message') ? params['head_commit']['message'] : nil
+        model = RepoMessage.new(action: request.headers['HTTP_X_GITHUB_EVENT'],
+                                description: message, body: params['head_commit'].to_s,
+                                status: 'recd')
+      else
+        model = RepoMessage.new(action: request.headers['HTTP_X_GITHUB_EVENT'],
+                                body: params.to_s, status: 'unk')
+      end
+      model.save!
+    end
     render text: 'OK'
   end
 end
--- db/migrate/20161111220423_create_repo_messages.rb
+++ db/migrate/20161111220423_create_repo_messages.rb
@@ -4,6 +4,11 @@ class CreateRepoMessages < ActiveRecord::Migration
       t.string  :description
       t.string  :action
       t.text    :body
+      t.string  :status, limit: 8
+      t.string  :new, array: true
+      t.string  :new_status, array: true
+      t.string  :changed, array: true
+      t.string  :changed_status, array: true
 
       t.timestamps null: false
     end
